21bd1d6cdcc3c8b9d3463d23e87a3f857b617dfd
41023125 <41023125@gm.nfu.edu.tw>
2023-06-04 22:31:18 +0800

    機械轉盤0604-2.ttt

--- downloads/1/car.py
+++ downloads/1/car.py
@@ -2,7 +2,7 @@
 #from zmqRemoteApi import RemoteAPIClient
 from zmqRemoteApi_IPv6 import RemoteAPIClient
 import keyboard
-client = RemoteAPIClient('192.168.1.24', 23000)
+client = RemoteAPIClient('127.0.0.1', 23000)
 
 print('Program started')
 sim = client.getObject('sim')
