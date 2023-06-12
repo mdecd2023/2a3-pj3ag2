0d9d98d3a60ac98d86da5e89ce64a792468a9743
urchen41023151 <urchen104023@gmail.com>
2023-06-12 11:07:00 +0800

    h1

--- downloads/1/car.py
+++ downloads/1/car.py
@@ -2,14 +2,14 @@
 #from zmqRemoteApi import RemoteAPIClient
 from zmqRemoteApi_IPv6 import RemoteAPIClient
 import keyboard
-client = RemoteAPIClient('127.0.0.1', 23000)
+client = RemoteAPIClient('2001:288:6004:17:2023:cda:2:5', 23000)
 
 print('Program started')
 sim = client.getObject('sim')
 sim.startSimulation()
 print('Simulation started')
 
-car = 4
+car = 10
 def setBubbleRobVelocity(leftfrontWheelVelocity, rightfrontWheelVelocity,leftbackWheelVelocity,rightbackWheelVelocity):
     leftfrontMotor = sim.getObject('/Shape['+str(car)+']/LF')
     rightfrontMotor = sim.getObject('/Shape['+str(car)+']/RF')
